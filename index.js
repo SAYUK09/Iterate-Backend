2b9591433d1005f9fc71ab9421f47d965e16cf0e
Sayuri <sayurikamble123@gmail.com>
2024-02-10 16:17:56 +0530

    feat: built `relevant-products` route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const OpenAI = require("openai");
+const fs = require("fs");
 require("dotenv").config();
 
 const app = express();
@@ -12,6 +13,8 @@ const client = new OpenAI({ apikey: process.env.OPENAI_API_KEY });
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
+
+const products = JSON.parse(fs.readFileSync("database/products.json", "utf8"));
 
 app.post("/generate-image", async (req, res) => {
   const prompt = req.body.prompt;
@@ -27,6 +30,51 @@ app.post("/generate-image", async (req, res) => {
 
     const imageUrl = response.data[0].url;
     res.json({ image_url: imageUrl });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/relevant-products", async (req, res) => {
+  const inputString = req.body.product_idea;
+
+  try {
+    const response = await client.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "Please classify the category of this product. You can also classify the product into suitable multiple category",
+        },
+        { role: "user", content: inputString },
+      ],
+      max_tokens: 100,
+    });
+
+    const category = response.choices[0].message.content;
+    const categories = category
+      .split(" ")
+      .map((cat) => cat.trim().toLowerCase());
+
+    console.log(categories, "category");
+
+    const matchingProducts = products.filter((product) => {
+      const productCategories = product.category.map((category) =>
+        category.toLowerCase()
+      );
+
+      return categories.some((cat) => productCategories.includes(cat));
+    });
+
+    if (matchingProducts.length > 0) {
+      res.json({ products: matchingProducts, message: "Products Found" });
+    } else {
+      res.json({
+        products: matchingProducts,
+        message: "Your idea is unique, no such idea in the database",
+      });
+    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
